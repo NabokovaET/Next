c2df696036dde3548c42a956f097765beccfd7d5
Nabokova Olga <olga.nabokova@fojin.tech>
2022-05-23 17:37:16 +0300

    add MobX

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,10 +1,43 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import styled from "@emotion/styled";
 import Heading from "../../components/Heading";
 import { contactType } from "../../components/types";
-import styled from "@emotion/styled";
+import contactsStore from "../../store/contactsStore";
+import { observer } from "mobx-react-lite";
+
+
+const ContactsTitle = styled.div`
+    display: flex;
+    align-items: center;
+    & > h1 {
+        margin-right: 1rem;
+    }
+`
+
+const ContactsButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: bisque;
+    width: 2rem;
+    height: 2rem;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+`
+
+const ContactsForm = styled.form`
+    & > input {
+        width: 100%;
+        font-size: 1.2rem;
+        margin: 0.3rem;
+        padding: 0.5rem;
+        border: 1px solid #d94821;
+    }
+`
 
 const ContactsList = styled.ul`
     display: flex;
@@ -46,22 +79,48 @@ export const getStaticProps:GetStaticProps = async () => {
     }
 }
 
-const Contacts:FC<ContactsTypeProps> = ({ contacts }) => {
+const Contacts:FC<ContactsTypeProps> = observer(({ contacts }) => {
+    const [value, setValue] = useState<string>('');
+    const [isActive, setIsActive] = useState<boolean>(false);
+
+    useEffect(() => {
+        contactsStore.getContacts(contacts);
+    }, [contacts])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        contactsStore.addContacts(value);
+        setValue('');
+        setIsActive(false);
+    }
+
     return (
         <>
-            <Head>      
-                <title>Contacts</title>
-            </Head>
-            <Heading text='Contacts list'/>
+            <Head><title>Contacts</title></Head>
+            <ContactsTitle>
+                <Heading text='Contacts list'/>
+                <ContactsButton onClick={() => setIsActive(!isActive)}>+</ContactsButton>
+            </ContactsTitle>
             <ContactsList>
-                {contacts && contacts.map(({ id, name }) => (
+                {isActive ? 
+                    <ContactsForm onSubmit={(e) => handleSubmit(e)}>
+                        <input onChange={(e) => setValue(e.target.value)}/> 
+                    </ContactsForm>
+                    : null
+                }
+                {contactsStore.contacts.map(({ id, name }) => (
                     <ContactsItem key={id}>
                         <Link href={`/contacts/${id}`}>{name}</Link>
                     </ContactsItem>
                 ))}
+                {/* {contacts && contacts.map(({ id, name }) => (
+                    <ContactsItem key={id}>
+                        <Link href={`/contacts/${id}`}>{name}</Link>
+                    </ContactsItem>
+                ))} */}
             </ContactsList>
         </>
     )
-};
+});
    
 export default Contacts;
